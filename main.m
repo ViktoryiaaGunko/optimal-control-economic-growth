3b3ed1ee90ad71c43a189629aeae8aa63586454c
Gunko Vika <vikahunko@icloud.com>
2026-04-22 19:27:19 +0300

    Extract optimal control and recalculate capital trajectory

--- main.m
+++ main.m
@@ -1,4 +1,4 @@
-% --- ПАРАМЕТРЫ МОДЕЛИ ---
+% ПАРАМЕТРЫ МОДЕЛИ 
 T = 10;          
 N = 30;          % Уменьшили количество точек для скорости fminsearch
 dt = T / (N-1);  
@@ -10,7 +10,7 @@ rho = 0.05;
 epsilon = 0.01;  
 z0 = 2.0;        
 
-% --- НАСТРОЙКИ ОПТИМИЗАЦИИ (БАЗОВЫЙ MATLAB - fminsearch) ---
+% --- НАСТРОЙКИ ОПТИМИЗАЦИИ (MATLAB - fminsearch) ---
 % Чтобы ограничить u от 0 до 0.99, мы оптимизируем переменную v
 % Связь: u = 0.99 / (1 + exp(-v))
 u_init = 0.11; % Стартуем с магистрали
@@ -21,3 +21,14 @@ options = optimset('Display', 'iter', 'MaxIter', 5000, 'MaxFunEvals', 20000);
 
 % Запуск оптимизации
 v_opt = fminsearch(@(v) cost_function_unconstrained(v, N, dt, t, alpha, rho, mu, z0, epsilon), v0, options);
+
+% ВОССТАНОВЛЕНИЕ РЕЗУЛЬТАТОВ 
+% Возвращаем u_opt в диапазон [0, 0.99]
+u_opt = (1 - epsilon) ./ (1 + exp(-v_opt));
+
+% Прямой расчет z_opt по найденному u_opt
+z_opt = zeros(1, N);
+z_opt(1) = z0;
+for i = 1:N-1
+    z_opt(i+1) = z_opt(i) + dt * (u_opt(i) * z_opt(i)^alpha - mu * z_opt(i));
+end
